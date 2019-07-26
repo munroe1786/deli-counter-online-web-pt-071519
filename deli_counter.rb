19310802f8f1b839d6a79cf7842f05c3ff2f3532
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 14:16:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(array)
     puts "The line is currently empty."
   else 
     array.each_with_index do |name, index|
-    puts "The line is currently: {customer_list << #{index+1}.#{name}."
+    puts "The line is currently: #{customer_list << #{index+1}.#{name}."}
 end
 end
 end
