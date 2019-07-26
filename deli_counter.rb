01a9f8253a22aa81e3fefa3bef98c883e85fa5df
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 15:08:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+#split array into a string, insert index +1 numbers for line #s#
+
+
+def line(array)
+  katz_deli = [ ]
+  if array.length == 0
+    puts "The line is currently empty."
+  else 
+    array.each_with_index do |name, index|
+    katz_deli.push ("#{index+1}. #{name}")
+  end
+    puts "The line is currently: #{katz_deli.join(" ")}"
+end
+end
+
+def take_a_number(array, string)
+  array.push(string)
+  puts "Welcome, #{string}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.length == 0
+    puts "There is nobody waiting to be served!"
+  else  
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
+
+  
