2937fb687cc2335e6e0aa3b919750864d3f37da5
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 14:10:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@
 
 def line(array)
   customer_list = array.join(" ")
-  if array.length == 0
+  if customer_list.length == 0
     puts "The line is currently empty."
   else 
     array.each_with_index do |name, index|
