e978a634113a1fc643a9eb4eb814fc42e6d7ee94
Eliza Munroe <era3munroe@gmail.com>
2019-07-26 14:07:09 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+#split array into a string, insert index +1 numbers for line #s#
+
+
+def line(array)
+  customer_list = array.join(" ")
+  if array.length == 0
+    puts "The line is currently empty."
+  else 
+    array.each_with_index do |name, index|
+    customer_list >> "The line is currently: #{index+1}.#{name}."
+    puts customer_list
+end
+end
